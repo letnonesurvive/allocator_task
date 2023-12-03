30beb8a0bc2574b46ed066c51568b2486b59efad
letnonesurvive <hippiman1221@gmail.com>
2023-12-03 15:15:34 +0000

    added allocator

--- Allocator.h
+++ Allocator.h
@@ -1,7 +1,36 @@
 #pragma once
 
-template<class Type, typename N>
+#include <memory>
+
+template<class Type>
 class Allocator
 {
 
+public:
+
+    using value_type = Type;
+
+    Allocator() = default;
+
+
+    Type* allocate (size_t N)
+    {
+        if (N > myMaxSize) {
+            throw std::exception ("The memory allocation limit for the number of items has been exceeded");
+        }
+
+        if (void* aPointer = std::malloc (N * sizeof (Type))) {
+            return static_cast<Type*> (aPointer);
+        }
+        throw std::bad_alloc();
+    }
+
+    void deallocate (Type* thePointer, size_t N) noexcept
+    {
+        (void)N;
+        std::free (thePointer);
+    }
+
+private:
+    size_t myMaxSize = 100;
 };
--- main.cpp
+++ main.cpp
@@ -2,26 +2,31 @@
 #include <map>
 
 #include "Container.h"
+#include "Allocator.h"
 
 using namespace std;
 
 int main()
 { 
-    container<int> aContainer;
-    aContainer.push_back(1);
-    aContainer.push_back(2);
-    aContainer.push_back(3);
+    try {
+        container<int, Allocator<int>> aContainer (30);
 
-    while (aContainer.has_next()) {
-        cout << aContainer.next() << endl;
+        aContainer.push_back(1);
+        aContainer.push_back(2);
+        aContainer.push_back(3);
+
+        while (aContainer.has_next()) {
+            cout << aContainer.next() << endl;
+        }
+
+    } catch (exception anException) {
+        cout << anException.what();
     }
-    /*container<int> aContainer (4);
-    aContainer.push_back (1);
-    aContainer.push_back (2);
 
-    while (aContainer.has_next()) {
+    
+
+    /*while (aContainer.has_next()) {
         cout << aContainer.next() << endl;
     }*/
-
     
 }
